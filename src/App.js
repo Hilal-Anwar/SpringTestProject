d65a98ac43be83c0a510bd7ea162ab39c9d6fd47
Helal Anwar <hilal123anwar@gmail.com>
2023-09-08 01:47:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/groups" exact={true} element={<GroupList/>}/>
+        <Route path="/groups/:id" exact={true} element={<GroupEdit/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/AppNavbar.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const AppNavbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Navbar color="dark" dark expand="md">
+      <NavbarBrand tag={Link} to="/">Graded
+        </NavbarBrand>
+      <NavbarToggler onClick={() => {
+        setIsOpen(!isOpen)
+      }}/>
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="justify-content-end" style={{ width: "100%" }} navbar>
+          <NavItem>
+            <NavLink href="https://gradeclasses.com/">@graded</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/oktadev/okta-spring-boot-react-crud-example">GitHub</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
